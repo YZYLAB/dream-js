7eb771a3d0142cf3190f8374e76fbc2df65a68cc
YZYLAB <general@yzylab.com>
2020-02-11 11:28:31 +0100

    Undetected (Thanks to Puppeteer Extra Plugin Stealth)

--- example.js
+++ example.js
@@ -1,14 +1,16 @@
 const Dream = require('./index').default
 const dream = new Dream({
     show: true,
-    debug: false,
+    openDevTools: { detach: true }
 })
 
 dream
-    .goto('https://www.google.com')
+    .useragent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36')
+    .goto('https://bot.sannysoft.com/')
     .evaluate(function(){
         return document.body.innerHTML;
     })
+    .wait(100000000000)
     .end()
     .then(function(result) {
         console.log(result)
